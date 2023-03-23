4ebb78ac1ce2ec3561189b6491c3a8e1355bc2ab
Saulo Henrique <saulohjr@hotmail.com>
2023-03-22 22:52:33 -0300

    controller e service

--- src/libs/Math.test.ts
+++ src/libs/Math.test.ts
@@ -1,6 +1,12 @@
 import { Math } from './Math';
 
 describe('Testando a biblioteca Math', () => {
+
+    //beforeEach(() => {  }); executa antes de cada teste
+    //afterEach(()=> { });  //executa depois de cada teste  
+    // beforeAll(() => {}); // executa antes de todos os teste
+    // afterAll() => {}); // executa depois de todos os tentes
+
     it('should sum two number correctly', () => {
         const response = Math.sum(5, 10);
         expect(response).toBe(15);
@@ -26,5 +32,35 @@ describe('Testando a biblioteca Math', () => {
         const response2 = Math.div(3, 0);
         expect(response2).toBe(false);
 
+    });
+
+    it('contar quantos caracteres tem na string', () => {
+        const response = 'saulo'.length;
+        expect(response).toHaveLength(5);
     })
-})
+
+    it('se tem a propriedade EMAIL', () => {
+        const response = {
+            name: 'saulo',
+            email: 'saulohjr@gmail.com'
+        };
+        expect(response).toHaveProperty('email');
+    })
+
+    it('se tem a propriedade igual', () => {
+        const response = 20;
+        expect(response).toEqual(20);
+    })
+
+    it('se tem a propriedade maior que', () => {
+        const response = 20;
+        expect(response).toBeGreaterThan(20);
+    });
+
+    it('se tem a propriedade email no padrão', () => {
+        const response = 'saulohjr@hotmail.com';
+        expect(response).toMatch(/[a-z]@[a-z].[a-z]/);
+    });
+
+
+});
